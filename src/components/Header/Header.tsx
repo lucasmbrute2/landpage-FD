7df77f197904e241cc481da38e61bd37c0dfd9d9
Lucas Dantas <lucasmbrute614@gmail.com>
2022-08-10 00:00:25 -0300

    feat: implemented mobile menu bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
 import "./Header.scss"
 import image1 from "../../assets/logo-in8-dev.svg"
+import { useState } from "react"
 
 export function Header() {
+
+  const [toggle, setToggle] = useState(false)
+
+  function handleMenuBar() {
+    setToggle(!toggle)
+  }
+
   return (
     <div className="header">
       <div className="background-image"></div>
@@ -10,13 +18,16 @@ export function Header() {
           <img src={image1} alt='logo' className='logo' />
         </div>
         <div className="header-container-nav">
-          <ul className="header-container-nav-ul">
-            <li className='header-container-nav-li'>Cadastro</li>
-            <li className="separator"></li>
-            <li className='header-container-nav-li'>lista</li>
-            <li className="separator"></li>
-            <li className='header-container-nav-li'>Sobre mim</li>
+          <ul className={toggle ? "responsiveList" : "header-container-nav-ul"}>
+            <li className={toggle ? "li--active" : 'header-container-nav-li'}>Cadastro</li>
+            <li className={toggle ? "li--active" : "separator"}></li>
+            <li className={toggle ? "li--active" : 'header-container-nav-li'}>lista</li>
+            <li className={toggle ? "li--active" : "separator"}></li>
+            <li className={toggle ? "li--active" : 'header-container-nav-li'}>Sobre mim</li>
           </ul>
+        </div>
+        <div className={toggle ? "icon iconActive" : "icon"} onClick={handleMenuBar}>
+          <div className="hamburguer hamburguerIcon"></div>
         </div>
       </div>
       <div className="header-tittle">
